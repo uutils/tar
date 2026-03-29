5ac4df64bdd3ad4844d14a74551181e1a0b2bb95
oech3 <79379754+oech3@users.noreply.github.com>
2026-03-30 01:28:11 +0900

    GnuTests: support any build profile

--- util/build-gnu.sh
+++ util/build-gnu.sh
@@ -5,28 +5,20 @@ ME="${0}"
 ME_dir="$(dirname -- "$("${READLINK:-readlink}" -fm -- "${ME}")")"
 REPO_main_dir="$(dirname -- "${ME_dir}")"
 
-# Default profile is 'debug'
-UU_MAKE_PROFILE='debug'
-
-for arg in "$@"
-do
-    if [ "$arg" == "--release-build" ]; then
-        UU_MAKE_PROFILE='release'
-        break
-    fi
-done
+: ${PROFILE:=debug} # default profile
+export PROFILE
 
 path_UUTILS=${path_UUTILS:-${REPO_main_dir}}
 path_GNU="${path_GNU:-${path_UUTILS}/../gnu}"
 
 echo "Building uutils tar..."
 cd "${path_UUTILS}"
-cargo build --profile "${UU_MAKE_PROFILE}" --bin tarapp
+cargo build --profile="${PROFILE}" --bin tarapp
 
 if [[ ! -z  "$CARGO_TARGET_DIR" ]]; then
-    UU_BUILD_DIR="${CARGO_TARGET_DIR}/${UU_MAKE_PROFILE}"
+    UU_BUILD_DIR="${CARGO_TARGET_DIR}/${PROFILE}"
 else
-    UU_BUILD_DIR="${path_UUTILS}/target/${UU_MAKE_PROFILE}"
+    UU_BUILD_DIR="${path_UUTILS}/target/${PROFILE}"
 fi
 
 # Symlink tarapp to tar so tests find it as 'tar'
